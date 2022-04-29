b0aa1b6d0323a99892b161265d754a8397e8ec9a
lookieAU <plabandatta2015@gmail.com>
2022-04-29 14:01:54 +0530

    React commit 2 Linux LAB

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class Car extends React.Component{
-  render(props){
-    return <h1>Hello {props.color}</h1>
+  constructor(props){
+    super(props);
+    this.state ={year: "2022", brand: "TVS"};
+
+  }
+  changefunc = () => {
+    this.setState({year: "2010"});
+  }
+  render(){
+    return(
+      <>
+      <div>
+        <h1>Car list {this.state.year} and {this.state.brand}</h1>
+        <button type="button" onClick={this.changefunc}>Change Year</button>
+      </div>
+      <form >
+        <label for = "name">Enter your Name:</label>
+        <input type = "text"  name = "name"/>
+        <button type = "submit">Submit the form</button>
+      </form>
+      </>
+    );
   }
 }
 
